4945d59fc419f5e176b31a9d719c94dc234f5221
AviadSar <aaviada@gmail.com>
2022-01-03 15:33:24 +0200

    -misc

---
+++ runner.py
@@ -0,0 +1,6 @@
+import os
+
+# os.system("python3 trainer.py --json_file trainer_args/home/snli/roberta-base/roberta-base/1/whole_set/args.json")
+# os.system("python3 trainer.py --json_file trainer_args/home/snli/roberta-base/roberta-base/0.33/random/args.json")
+os.system("python3 trainer.py --json_file trainer_args/hadassah/snli/roberta-base/roberta-base/1/whole_set/args.json")
+os.system("python3 trainer.py --json_file trainer_args/hadassah/snli/roberta-base/roberta-base/0.33/random/args.json")
