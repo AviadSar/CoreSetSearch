c0074d90e2b142952e8c917449dd14fff87d5656
AviadSar <aaviada@gmail.com>
2022-01-04 11:10:59 +0200

    -misc

--- core_set_finder.py
+++ core_set_finder.py
@@ -4,12 +4,14 @@ import datasets
 import time
 import dataset_classes
 import os
+import pickle
 from model_loader import get_model_and_tokenizer_from_args
 from data_tokenizer import tokenize
 from greedy_k_means import GreedyKMeans
 
 
 CORE_SET_BASE_DIR = 'core_sets'
+HIDDEN_STATES_BASE_DIR = 'hidden_states'
 
 
 def get_core_set_dir_from_args(args):
@@ -19,6 +21,14 @@ def get_core_set_dir_from_args(args):
             os.makedirs(core_set_dir)
             print("creating core set directory " + core_set_dir)
         return core_set_dir
+
+
+def get_hidden_states_dir_from_args(args):
+    hidden_states_dir = os.path.join(HIDDEN_STATES_BASE_DIR, args.dataset_name, args.core_set_model_name)
+    if not os.path.isdir(hidden_states_dir):
+        os.makedirs(hidden_states_dir)
+        print("creating core set directory " + hidden_states_dir)
+    return hidden_states_dir
 
 
 def get_hidden_states_from_tokens(model, tokenized_dataset, args):
@@ -42,9 +52,20 @@ def get_hidden_states_from_tokens(model, tokenized_dataset, args):
 
 
 def get_hidden_states(dataset, ratio, args):
+    hidden_states_dir = get_hidden_states_dir_from_args(args)
+    files = os.listdir(hidden_states_dir)
+
+    for file in files:
+        if file == 'hidden_states.pkl':
+            with open(os.path.join(hidden_states_dir, file), 'rb') as pkl_file:
+                return pickle.load(pkl_file)
+
     model, tokenizer = get_model_and_tokenizer_from_args(args)
     tokenized_dataset = tokenize(dataset['train'], tokenizer, args.dataset_name, ratio=ratio)
     hidden_states = get_hidden_states_from_tokens(model, tokenized_dataset, args)
+
+    with open(os.path.join(hidden_states_dir, 'hidden_states.pkl'), 'wb') as pkl_file:
+        pickle.dump(hidden_states, pkl_file)
 
     return hidden_states
 
@@ -56,18 +77,22 @@ def random_core_set(dataset, args):
     return dataset
 
 
+def core_set_by_method(dataset, method, args):
+    ratio = 1
+    hidden_states = get_hidden_states(dataset, ratio, args)
+    method = method(hidden_states)
+    core_set_idx, cover_distance = method.sample([], int(len(dataset['train']) * ratio * args.core_set_size))
+
+    dataset['train'] = dataset['train'].select(core_set_idx)
+    dataset['train'].flatten_indices()
+
+
 def greedy_k_means_core_set(dataset, args):
     core_set_dir = get_core_set_dir_from_args(args)
     if any(os.scandir(core_set_dir)):
         dataset['train'] = datasets.load_from_disk(core_set_dir)
     else:
-        ratio = 1
-        hidden_states = get_hidden_states(dataset, ratio, args)
-        method = GreedyKMeans(hidden_states)
-        core_set_idx, cover_distance = method.sample([], int(len(dataset['train']) * ratio * args.core_set_size))
-
-        dataset['train'] = dataset['train'].select(core_set_idx)
-        dataset['train'].flatten_indices()
+        core_set_by_method(dataset, GreedyKMeans, args)
         try:
             dataset['train'].save_to_disk(core_set_dir)
         except Exception as e:
